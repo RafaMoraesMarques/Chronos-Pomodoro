ee69bf2c37af0f1378f9f2bc3f35844f2eb3d85c
RafaelMarques <rafammarques21@gmail.com>
2025-09-05 16:39:02 -0300

    Primeiro commit

---
+++ src/contexts/TaskContext/TaskContext.tsx
@@ -0,0 +1,17 @@
+import { createContext } from 'react';
+import { initialTaskState } from './initialTaskState';
+import type { TaskActionModel } from './taskActions';
+import type { TaskStateModel } from '../../models/TaskStateModel';
+
+
+type TaskContextProps = {
+  state: TaskStateModel;
+  dispatch: React.Dispatch<TaskActionModel>;
+};
+
+const initialContextValue = {
+  state: initialTaskState,
+  dispatch: () => {},
+};
+
+export const TaskContext = createContext<TaskContextProps>(initialContextValue);
